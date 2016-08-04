41ccf93a2b1e50836cbef831e775f7192629d5b9
Mike Trupkin <mtrupkin@s4isystems.com>
2016-08-04 14:17:21 -0700

    wip optimization

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
-ARTIFACT='DGR'
+ARTIFACT=DGR
+TARGET=./target/$ARTIFACT.DSK
 
-MERLIN_HOME='../Merlin32_v1.0'
-MERLIN_CMD=$MERLIN_HOME'/Windows/Merlin32.exe -V '$MERLIN_HOME'/Library'
+APPLE_HOME=/c/util-apple
 
-CADIUS_HOME='../Cadius_v1.1'
-CADIUS_TARGET='./target/'$ARTIFACT'.2MG'
-CADIUS_CMD=$CADIUS_HOME'/Cadius.exe ADDFILE ./target/'$ARTIFACT'.2MG'
-#../Cadius_v1.1/Cadius.exe ADDFILE ./target/DGR.2MG DGR ./target/DGR
+MERLIN_HOME=$APPLE_HOME/Merlin32_v1.0
+CADIUS_HOME=$APPLE_HOME/Cadius_v1.1
+APPLEWIN_HOME=$APPLE_HOME/AppleWin1.25.0.4
+
+CADIUS_TARGET=./target/$ARTIFACT.2MG
+
+MERLIN_CMD="$MERLIN_HOME/Windows/Merlin32.exe -V $MERLIN_HOME/Library"
+CADIUS_CMD="$CADIUS_HOME/Cadius.exe ADDFILE $CADIUS_TARGET"
+APPLEWIN_CMD="$APPLEWIN_HOME/Applewin.exe -no-printscreen-dlg"
 
 rm -rf ./target
 mkdir ./target
@@ -16,11 +21,11 @@ cp -a ./src/build/. ./target/
 for f in ./src/main/6502/*.s; do
   $MERLIN_CMD $f
   OBJ=$(basename $f .s)
-  OBJ_TARGET='./target/'$OBJ
+  OBJ_TARGET=./target/$OBJ
   mv ./src/main/6502/$OBJ $OBJ_TARGET
   $CADIUS_CMD $ARTIFACT $OBJ_TARGET
 done
 
-mv ./target/$ARTIFACT.2MG ./target/$ARTIFACT.DSK
+mv $CADIUS_TARGET $TARGET
 
-start ./target/$ARTIFACT.DSK
+start $APPLEWIN_CMD -d1 $(realpath $TARGET)
--- src/main/6502/DGR.s
+++ src/main/6502/DGR.s
@@ -1,175 +1,139 @@
 ****************************************
 * Double Lo-Res Graphics
 ****************************************
+                ORG 	$6000
 
-                ORG $6000
+TEMP            EQU 	$E3 
+COLOR           EQU 	$FF 
 
-PLOTX           EQU $FD                
-PLOTY           EQU $FE
-COLOR           EQU $FF 
+**************************************************
+* Apple Standard Memory Locations
+**************************************************
+CLRLORES     	EQU 	$F832
+LORES        	EQU 	$C050
+TXTSET       	EQU 	$C051
+MIXCLR       	EQU 	$C052
+MIXSET       	EQU 	$C053
+TXTPAGE1     	EQU 	$C054
+TXTPAGE2     	EQU 	$C055
+KEY          	EQU 	$C000
+C80STOREOFF  	EQU 	$C000
+C80STOREON   	EQU 	$C001
+STROBE       	EQU 	$C010
+SPEAKER      	EQU 	$C030
+VBL          	EQU 	$C02E
+RDVBLBAR     	EQU 	$C019       ;not VBL (VBL signal low	
+RAMWRTAUX    	EQU 	$C005
+RAMWRTMAIN   	EQU 	$C004
+SETAN3       	EQU 	$C05E       ;Set annunciator-3 output to 0
+SET80VID     	EQU 	$C00D 		;enable 80-column display mode (WR-only)
 
-TMP             EQU $E3
-PTR             EQU $06
-LOMEM           EQU $300
-
+	
 ****************************************
 * Routine Table
 ****************************************
-                JMP EnableDGR
-                JMP ClearDGR
-                JMP PlotDGR
-                JMP HLine
-                JMP VLine
-                JMP Test1
+                JMP 	EnableDGR
+                JMP 	ClearDGR
+                JMP 	PlotDGR
+                JMP 	HLine
+                JMP 	VLine
+                JMP 	Test1
+                JMP 	Test2
  
 HLine           RTS 
 VLine           RTS
 
-Test1           LDA #$0F
-                STA COLOR
-:LOOP           JSR ClearDGR
-                DEC COLOR
-                BPL :LOOP
+Test1           LDA 	#$0F
+                STA 	COLOR
+                
+:LOOP 			JSR 	VBlank
+				JSR 	ClearDGR
+
+				LDX 	#5 	
+:PAUSE          JSR 	VBlank
+				DEX	
+	            BNE 	:PAUSE
+                DEC 	COLOR
+                BNE 	:LOOP
                 RTS
 
+Test2           JMP		Test1          
+
+Test3           LDA 	#$05
+                STA 	COLOR
+                JSR 	VBlank
+				JSR 	ClearDGR 
+                RTS
 
 ****************************************
 * Enable Double Lo-Res Mode
 ****************************************
-EnableDGR       STA   $C050 ; GRAPHICS         
-                STA   $C053 ; TEXT MIXED 
-                STA   $C054 ; PAGE ONE         
-                STA   $C056 ; LO-RES           
-                STA   $C05E ; TURN ON DOUBLE RES     
-
-* Copy text offsets to a low page memory
-
-                LDX #0 ; LOAD TEXT OFFSETS 
-:LOOP           LDA DATA1, X
-                STA LOMEM, X
-                LDA DATA1+1, X
-                STA LOMEM+1, X
-                INX
-                INX
-                CPX #40
-                BNE :LOOP
-    
-                RTS
+EnableDGR       STA 	LORES 		; GRAPHICS         
+				STA 	MIXSET 		; TEXT MIXED 
+                STA 	SETAN3 		; TURN ON DOUBLE RES     
+                STA 	SET80VID    ; ENABLE 80 COL DISPLAY 
+                STA 	C80STOREON  ; enable aux/page1,2 mapping        
 
 ****************************************
 * Plot
 ****************************************
 PlotDGR         RTS
                 
-                LDA PLOTY
-                LSR 
-                TAX    
-                LDA LOMEM, X
-                STA $06
-                LDA LOMEM+1, X
-                STA $07
-
 ****************************************
 * Clear Screen 
 ****************************************
-ClearDGR        LDA #0
-                STA $C054 ; MAIN MEM
-
-                DupNibbleMem  COLOR
-                CLR 
-
-                LDA #0
-                STA $C055 ; AUX MEM
-
-                LDY COLOR
-                LDA ColorTable, Y
-                DupNibbleAcc                            
-                CLR 
+ClearDGR        STA 	TXTPAGE1	; MAIN MEM
+				DupNibbleMem COLOR
+				ClearScreen                 
+                LDY 	COLOR
+                LDA 	ColorTable, Y
+				STA 	TXTPAGE2	; AUX MEM
+                DupNibbleAcc  
+                ClearScreen 
                 RTS
 
 ****************************************
 * Clear text memory page
 ****************************************
-CLR             MAC
+ClearScreen     MAC
 * X register contains current row 
-                LDX #16
-:ROW            DEX
-                DEX            
-
-                STA TMP ; SAVE ACC
-                LDA LOMEM, X
-                STA PTR
-                LDA LOMEM+1, X
-                STA PTR+1
-                LDA TMP ; RESTORE ACC                
-
-                LDY #120
-* Skip last 4 text rows (mixed gr mode)
-                CPX #8
-                BMI :COL
-                LDY #80
-
-:COL            DEY
-                STA (PTR), Y
-                BNE :COL
-
-                CPX #0
-                BNE :ROW
+                LDX 	#40 	
+LOOP            DEX
+                STA 	Lo01,x
+                STA 	Lo02,x
+                STA 	Lo03,x
+                STA 	Lo04,x
+                STA 	Lo05,x
+                STA 	Lo06,x
+                STA 	Lo07,x
+                STA 	Lo08,x
+                STA 	Lo09,x
+                STA 	Lo10,x
+                STA 	Lo11,x
+                STA 	Lo12,x
+                STA 	Lo13,x
+                STA 	Lo14,x
+                STA 	Lo15,x
+                STA 	Lo16,x
+                STA 	Lo17,x
+                STA 	Lo18,x
+                STA 	Lo19,x
+                STA 	Lo20,x
+                BNE 	LOOP
                 <<<
 
-****************************************
-* Clear Screen 2
-****************************************
-ClearDGR2       LDA #0
-                STA $C054 ; MAIN MEM
 
-                LDA COLOR
-                DupNibbleMem  COLOR
-                CLR2 
-
-                LDA #0
-                STA $C055 ; AUX MEM
-
-                LDY COLOR
-                LDA ColorTable, Y
-                DupNibbleAcc                            
-                CLR2 
-                RTS
-
-****************************************
-* Clear text memory page
-* X register contains current row
-****************************************
-CLR2            MAC
-                LDX #20
-:ROW            DEX
-  
-                PHA ; SAVE ACC
-                LDA $300, X
-                STA $06
-                LDA $301, X
-                STA $07
-                PLA ; RESTORE ACC
-            
-                LDY #40
-
-:COL            DEY
-                STA ($06), Y
-                BNE :COL
-                CPX #0
-                BNE :ROW
-                <<<
 ****************************************
 * Duplicate lo nibble to hi nibble
 * accumulator 
 ****************************************
 DupNibbleAcc    MAC 
-                STA TMP
+                STA TEMP
                 ASL
                 ASL
                 ASL
                 ASL
-                ORA TMP
+                ORA TEMP
                 <<<                
 
 ****************************************
@@ -185,10 +149,51 @@ DupNibbleMem    MAC
                 ORA ]1
                 <<<                                
 
-DATA1           DA $0400, $0480, $0500, $0580, $0600
-                DA $0680, $0700, $0780, $0428, $04A8 
-                DA $0528, $05A8, $0628, $06A8, $0728 
-                DA $07A8, $0450, $04D0, $0550, $05D0 
-                DA $0650, $06D0, $0750, $07D0
+****************************************
+* Wait for Vertical Blank 
+****************************************
+VBlank  		LDA 	#$7e
+:vblActive      CMP 	RDVBLBAR 	; make sure we wait for the current one to stop
+                BPL 	:vblActive 	; in case it got launched in rapid succession
+:screenActive   CMP 	RDVBLBAR
+                BMI 	:screenActive
+                RTS                
+
+
+**************************************************
+* Lores/Text lines
+**************************************************
+Lo01            EQU 	$0400
+Lo02            EQU 	$0480
+Lo03            EQU 	$0500
+Lo04            EQU 	$0580
+Lo05            EQU 	$0600
+Lo06            EQU 	$0680
+Lo07            EQU 	$0700
+Lo08            EQU 	$0780
+Lo09            EQU 	$0428
+Lo10            EQU 	$04A8
+Lo11            EQU 	$0528
+Lo12            EQU 	$05A8
+Lo13            EQU 	$0628
+Lo14            EQU 	$06A8
+Lo15            EQU 	$0728
+Lo16            EQU 	$07A8
+Lo17            EQU 	$0450
+Lo18            EQU 	$04D0
+Lo19            EQU 	$0550
+Lo20            EQU 	$05D0
+* the "plus four" lines
+Lo21            EQU 	$0650
+Lo22            EQU 	$06D0
+Lo23            EQU 	$0750
+Lo24            EQU 	$07D0
+
+LoLineTable     DA 		Lo01,Lo02,Lo03,Lo04,Lo05,Lo06
+                DA 		Lo07,Lo08,Lo09,Lo10,Lo11,Lo12
+                DA 		Lo13,Lo14,Lo15,Lo16,Lo17,Lo18
+                DA 		Lo19,Lo20,Lo21,Lo22,Lo23,Lo24
             
 ColorTable      DB 0,8,1,9,2,10,3,11,4,12,5,13,6,14,7,15
+
+
